9115ea2d5da2914f806545a6c07f04585c548823
Denis-us <75166604+Denis-us@users.noreply.github.com>
2021-05-29 11:20:21 +0300

    Делаю

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const colors = [
     '#795548',
   ];
 
+  const bodyEl = document.querySelector('body')
+  const buttonsEl = document.querySelectorAll('button')
+  // console.log(buttonsEl[0].dataset.action)
+  // console.log(buttonsEl[1].dataset.action)
+
+  // console.log(buttonsEl)
+
   const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
